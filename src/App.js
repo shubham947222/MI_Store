4d30ee7394a9d9d42a597a9d5f1c40b8a2bb4295
shubham947222 <shubham947222@gmail.com>
2022-03-08 12:25:45 +0530

    HotAccessories-section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import PreNavbar from "./components/PreNavbar";
+import Navbar from "./components/Navbar.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Slider from "./components/Slider.js";
+import Offers from "./components/Offers.js";
+import Heading from "./components/Heading.js";
+import StarProducts from "./components/StarProducts.js";
+import HotAccessoriesMenu from "./components/HotAccessoriesMenu.js";
+import HotAccessories from "./components/HotAccessories.js";
+import data from "./data/data.json";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <PreNavbar />
+      <Navbar />
+      <Slider start={data.banner.start} />
+      <Offers offers={data.offer} />
+      <Heading text="STAR PRODUCT" />
+      <StarProducts starProduct={data.starProduct} />
+      <Heading text="HOT ACCESSORIES" />
+      <HotAccessoriesMenu />
+      <Route exact path="/music">
+        <HotAccessories
+          music={data.hotAccessories.music}
+          musicCover={data.hotAccessoriesCover.music}
+        />
+      </Route>
+      <Route exact path="/smartDevice">
+        <HotAccessories
+          smartDevice={data.hotAccessories.smartDevice}
+          smartDeviceCover={data.hotAccessoriesCover.smartDevice}
+        />
+      </Route>
+
+      <Route exact path="/home">
+        <HotAccessories
+          home={data.hotAccessories.home}
+          homeCover={data.hotAccessoriesCover.home}
+        />
+      </Route>
+
+      <Route exact path="/lifestyle">
+        <HotAccessories
+          lifeStyle={data.hotAccessories.lifeStyle}
+          lifeStyleCover={data.hotAccessoriesCover.lifeStyle}
+        />
+      </Route>
+
+      <Route exact path="/mobileAccessories">
+        <HotAccessories
+          mobileAccessories={data.hotAccessories.mobileAccessories}
+          mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}
+        />
+      </Route>
+    </Router>
   );
 }
 
